be83bfd5a062a1af559caaa30f8d39126f54622c
Ilya <ilyabelevcev00@gmail.com>
2022-10-13 20:20:12 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// let input = document.querySelector('.input');
+// let btn = document.querySelector('.button');
+// let resultBlock = document.querySelector('.result-block');
+// let data = input.value;
+
+// function splitStr(str, separator) {
+//    let numbers = str.split(separator);
+//    for(let i = 0; i < numbers.length; i++) {
+//       numbers[i] = Number(numbers[i]);
+//    }
+//    let result = numbers[0] + (numbers[3] - numbers[1]) * numbers[2];
+//    resultBlock.innerHTML = result;
+// }
+
+// btn.addEventListener('click', function() {
+//    splitStr(data, " ")
+// });
